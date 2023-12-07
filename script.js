5695508eb0249767e2e1bff529afead689dc4a1f
tofu-buffet <153135029+tofu-buffet@users.noreply.github.com>
2023-12-06 17:18:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
         'ai-job-tb-framed.png',
     ];
 
+    
     const comicImg = document.getElementById('comic-img');
     const firstButton = document.getElementById('first');
     const prevButton = document.getElementById('prev');
