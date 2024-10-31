2002f6df551dc0ff411f2cb4aef044257f550d0b
tofu-buffet <153135029+tofu-buffet@users.noreply.github.com>
2024-10-31 09:56:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -239,8 +239,8 @@ document.addEventListener('DOMContentLoaded', function() {
         'covfefe-tb-framed.png',
         'treat-princess-tb-framed.png',
         'contrarian-tb-framed.png',
-/*
         'sugar-granny-tb-framed.png',
+/*
         'internet-of-things-tb-framed.png',
         'inheritance-tax-tb-framed.png',
         'behold-tb-framed.png',
