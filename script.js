38015a83a3ef4bad1bf1e60a311634f22b4c5213
tofu-buffet <153135029+tofu-buffet@users.noreply.github.com>
2025-02-12 09:28:08 -0800

    Update script.js

--- script.js
+++ script.js
@@ -291,8 +291,8 @@ document.addEventListener('DOMContentLoaded', function() {
         'safe-spaces-tb-framed.png',
         'joie-tb-framed.png',
         'red-flag-tb-framed.png',
-/*
         'earthworm-tb-framed.png',
+/*
         'garbage-novels-tb-framed.png',
         'underjoyed-tb-framed.png',
         'kudos-tb-framed.png',
