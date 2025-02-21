bed73d5158ad0cb0e23d6d17e21f369d96bcd77a
tofu-buffet <153135029+tofu-buffet@users.noreply.github.com>
2025-02-21 09:47:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -296,6 +296,7 @@ document.addEventListener('DOMContentLoaded', function() {
         'climate-denier-tb-framed.png',
         'slow-research-tb-framed.png',
         'goldfish-tb-framed.png',
+        'hard-to-be-a-woman-tb-framed.png',
 /*
         'underjoyed-tb-framed.png',
         'kudos-tb-framed.png',
