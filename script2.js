b28c5ba64507de8ef74e4089a2b02c1ad6654fbd
Kevin Knight <kevin.crawford.knight@gmail.com>
2025-04-25 17:18:06 -0700

    new

--- script2.js
+++ script2.js
@@ -73,7 +73,6 @@ document.addEventListener('DOMContentLoaded', function() {
 'joie-tb-framed.png',
 'work-life-tb-framed.png',
 'tofu-buffet-cover.png'
-*/
     ];
     currentIndex = comics.length - 1; 
     
